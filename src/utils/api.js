7c39bf8f57c65805e31a483b13f50711c9880a3d
Divyang Dodiya <devxdodiya@gmail.com>
2025-08-24 18:15:34 +0530

    added gitignore and  package update for react and lefleat and updated api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 // File: src/utils/api.js
 
-const BASE_API_URL = 'http://localhost/react-api';
-const BASE_IMAGE_URL = 'http://localhost/react-api/uploads/';
+const BASE_API_URL = 'https://api.franmaxindia.com';
+const BASE_IMAGE_URL = 'https://api.franmaxindia.com/uploads/';
 
 export const getApiUrl = (endpoint) => `${BASE_API_URL}/${endpoint}`;
 export const getImageUrl = (imagePath) => `${BASE_IMAGE_URL}${imagePath}`;
